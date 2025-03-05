cd7365b184dfa822563328cf42690faea2d0f5ca
Afonso Fernandes <afonsofernandes1@MacBook-Pro-de-Afonso.local>
2025-03-05 15:20:52 +0000

    Tabela adicionada , aspeto visual por ficar melhor definido

--- src/app/family/cards/page.js
+++ src/app/family/cards/page.js
@@ -7,7 +7,7 @@ export default function App() {
     return (
         <Card className="w-65 h-75 bg-white rounded-lg shadow-2xl border border-gray-300">
             <CardBody>
-                <p>Este card é para o dashboard</p>
+                <p>Isto é uma card</p>
             </CardBody>
         </Card>
     );
--- src/app/family/tabelas/page.js
+++ src/app/family/tabelas/page.js
@@ -1,9 +1,71 @@
 "use client"
 
-import Image from "next/image";
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell,
+    getKeyValue,
+} from "@heroui/react";
 
-export default function Home() {
+const rows = [
+    {
+        key: "1",
+        name: "Tony Reichert",
+        role: "CEO",
+        status: "Active",
+    },
+    {
+        key: "2",
+        name: "Zoey Lang",
+        role: "Technical Lead",
+        status: "Paused",
+    },
+    {
+        key: "3",
+        name: "Jane Fisher",
+        role: "Senior Developer",
+        status: "Active",
+    },
+    {
+        key: "4",
+        name: "William Howard",
+        role: "Community Manager",
+        status: "Vacation",
+    },
+];
+
+const columns = [
+    {
+        key: "name",
+        label: "NAME",
+    },
+    {
+        key: "role",
+        label: "ROLE",
+    },
+    {
+        key: "status",
+        label: "STATUS",
+    },
+];
+
+export default function App() {
     return (
-        <div>hello 4</div>
+        <Table aria-label="Example table with dynamic content" className={"border border-gray-300 bg-white shadow-md rounded-lg"}>
+            <TableHeader columns={columns}>
+                {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+            </TableHeader>
+            <TableBody items={rows}>
+                {(item) => (
+                    <TableRow key={item.key}>
+                        {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
     );
 }
+
